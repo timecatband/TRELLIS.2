d4c97f90b5159895ecbbd93ed054d7f4a9b99e3a
Robert Carr <racarr@Roberts-MacBook-Pro.local>
2026-02-08 23:54:29 -0800

    More setup fixes

--- setup_trellis.sh
+++ setup_trellis.sh
@@ -1,6 +1,8 @@
 sudo apt-get install -y libeigen3-dev
 ln -sf /usr/include/eigen3/Eigen /usr/include/Eigen
 pip install ninja
+pip install psutil
+pip install fastapi uvicorn
 cd TRELLIS.2
-bash setup.sh --basic --flash-attn --nvdiffrast --nvdiffrec --cumesh --o-voxel \
---flexgemm
+pip install flash-attn --no-build-isolation --extra-index-url https://pypi.nvidia.com
+bash setup.sh --basic --nvdiffrast --nvdiffrec --cumesh --o-voxel --flexgemm
