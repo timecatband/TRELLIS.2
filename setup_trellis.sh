f560a304eadc578be628d4db03ad978ef1bb80d9
Robert Carr <racarr@Roberts-MacBook-Pro.local>
2026-02-09 00:03:32 -0800

    More dumb fixes

--- setup_trellis.sh
+++ setup_trellis.sh
@@ -1,5 +1,6 @@
 # for 5090 series, 12.0 compute capability
 export TORCH_CUDA_ARCH_LIST="12.0"
+export NVCC_THREADS=2
 
 sudo apt-get install -y libeigen3-dev
 ln -sf /usr/include/eigen3/Eigen /usr/include/Eigen
