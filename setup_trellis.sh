e8a96667a42805ecc93260109a8c76eac9e2847a
Robert Carr <racarr@Roberts-MacBook-Pro.local>
2026-02-08 23:59:54 -0800

    lalalala

--- setup_trellis.sh
+++ setup_trellis.sh
@@ -1,8 +1,11 @@
+# for 5090 series, 12.0 compute capability
+export TORCH_CUDA_ARCH_LIST="12.0"
+
 sudo apt-get install -y libeigen3-dev
 ln -sf /usr/include/eigen3/Eigen /usr/include/Eigen
 pip install ninja
 pip install psutil
 pip install fastapi uvicorn
 cd TRELLIS.2
-pip install flash-attn --no-build-isolation --extra-index-url https://pypi.nvidia.com
+MAX_JOBS=12 pip install flash-attn --no-build-isolation --extra-index-url https://pypi.nvidia.com
 bash setup.sh --basic --nvdiffrast --nvdiffrec --cumesh --o-voxel --flexgemm
