a9404fc9929411bd648bc29f9ce95326dfd5ca4c
Robert Carr <racarr@Roberts-MacBook-Pro.local>
2026-02-27 22:56:56 -0800

    Hacks

--- trellis2/pipelines/rembg/BiRefNet.py
+++ trellis2/pipelines/rembg/BiRefNet.py
@@ -8,7 +8,9 @@ from PIL import Image
 class BiRefNet:
     def __init__(self, model_name: str = "ZhengPeng7/BiRefNet"):
         self.model = AutoModelForImageSegmentation.from_pretrained(
-            model_name, trust_remote_code=True
+            model_name, trust_remote_code=True,
+            low_cpu_mem_usage=False,
+            device_map=None
         )
         self.model.eval()
         self.transform_image = transforms.Compose(
